73fe555f7ba60d9634aeb4ecb378ba4517b21fc7
Pierre Castro <pierre.castro89@gmail.com>
2022-03-06 12:29:34 +0100

    added: day2 step2

--- Day2/day2.py
+++ Day2/day2.py
@@ -18,12 +18,32 @@ def step1(l):
         if movement == "forward":
             x += int(value)
         elif movement == "up":
-            y += -1 * int(value)
+            y -= int(value)
         else:
             y += int(value)
 
     return x * y 
 
+def step2(l):
+
+    x = 0
+    y = 0
+    aim = 0
+    movements = get_movements(l)
+
+    for movement, value in movements:
+        if movement == "forward":
+            x += int(value)
+            y += int(value) * aim
+        elif movement == "up":
+            aim -= int(value)
+        else:
+            aim += int(value)
+    
+    return x * y 
+
+
 with open('data.txt') as file:
     data = file.read().splitlines()
-    print(f"Horizontal position * depth = {step1(data)}")
+    print(f"Step1 : Horizontal position * depth = {step1(data)}")
+    print(f"Step2 : Horizontal position * depth = {step2(data)}")
