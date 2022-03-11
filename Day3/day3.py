976025b990999cfb906667c122fa1beea23ec06b
Pierre Castro <pierre.castro89@gmail.com>
2022-03-11 14:38:44 +0100

    chore(day3): changes requested by ade-sede

--- Day3/day3.py
+++ Day3/day3.py
@@ -1,23 +1,26 @@
 #!/usr/bin/env python3
 
 
-def step1(ll):
+def step1(bin_list):
     
     gamma = ''
     epsilon = ''
 
-    for bit in range(len(ll[0])):
-        sumof0 = sum(number[bit] == '0' for number in ll)
-        sumof1 = sum(number[bit] == '1' for number in ll)
-        if sumof0 > sumof1:
-            gamma += '0'
-            epsilon += '1'
-        else:
-            gamma += '1'
-            epsilon += '0'
+    for bit in range(len(bin_list[0])):
+        sum_of_0 = sum(number[bit] == '0' for number in bin_list)
+        sum_of_1 = sum(number[bit] == '1' for number in bin_list)
+
+        gamma += '0' if (sum_of_0 > sum_of_1) else '1'
+        epsilon += '1' if (sum_of_0 > sum_of_1) else '0'
+
         
     return int(gamma, 2) * int(epsilon, 2)
         
     
-def step2(ll):
+def step2(bin_list):
     return 0
+
+
+with open('data.txt', 'r') as file:
+    data = file.read().splitlines()
+    print(step1(data))
--- Day3/test.py
+++ Day3/test.py
@@ -1,10 +1,11 @@
-#!/usr/bin/env python3
-
+import unittest
 from day3 import step1, step2
 
-def test():
-    values = ["00100","11110","10110","10111","10101","01111","00111","11100","10000","11001","00010","01010"]
-    assert step1(values) == 198
-    print("Step 1 ok")
+class TestDay3(unittest.TestCase):
+    def test(self):
+        values = ["00100","11110","10110","10111","10101","01111","00111","11100","10000","11001","00010","01010"]
+        self.assertEqual(step1(values), 198)
 
-test()
+
+if __name__ == '__main__':
+    unittest.main()
